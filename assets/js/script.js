99c7cd812b0361984778cbc52ac240eb34d3cd1a
CarlosOrellana00 <carlosorellana.oa@gmail.com>
2023-05-05 00:54:26 -0400

    manifest.js

---
+++ assets/js/script.js
@@ -0,0 +1,60 @@
+const colorPickerBtn = document.querySelector("#color-picker");
+const clearAll = document.querySelector(".clear-all");
+const colorList = document.querySelector(".all-colors");
+const pickedColors = JSON.parse(localStorage.getItem("picked-colors") || "[]");
+
+//copiar el elemento a la tabla y actualizar la lista
+const copyColor = (elem) => {
+    elem.innerText = "Copied";
+    navigator.clipboard.writeText(elem.dataset.color);
+    setTimeout(() => elem.innerText = elem.dataset.color, 1000);
+}
+
+const showColor = () => {
+    if(!pickedColors.length) return; // volver atras si no hay colores seleccionados
+    colorList.innerHTML = pickedColors.map(color => `
+        <li class="color">
+            <span class="rect" style="background: ${color}; border: 1px solid ${color == "#ffffff" ? "#ccc": color}"></span>
+            <span class="value hex" data-color="${color}">${color}</span>
+        </li>
+    `).join(""); // // Generating li for the picked color and adding it to the colorList
+    document.querySelector(".picked-colors").classList.remove("hide");
+
+    // Add a click event listener to each color element to copy the color code
+    document.querySelectorAll(".color").forEach(li => {
+        li.addEventListener("click", e => copyColor(e.currentTarget.lastElementChild));
+    });
+}
+showColor();
+
+const activateEyeDropper = () => {
+    document.body.style.display = "none";
+    setTimeout(async () => {
+        try {
+            //abrir la lupa y obtener el color seleccionado
+            const eyeDropper = new EyeDropper();
+            const { sRGBHex } = await eyeDropper.open();
+            navigator.clipboard.writeText(sRGBHex);
+
+            //agregar el color a la lista si este no existe
+            if(!pickedColors.includes(sRGBHex)) {
+                pickedColors.push(sRGBHex);
+                localStorage.setItem("picked-colors", JSON.stringify(pickedColors));
+                showColor();
+            }
+        } catch (error) {
+            alert("Failed to copy the color code!");
+        }
+        document.body.style.display = "block";
+    }, 10);
+}
+
+//limpiar todos los colores, actualziar el local-storage, ocular los elementos de la lista de colores
+const clearAllColors = () => {
+    pickedColors.length = 0;
+    localStorage.setItem("picked-colors", JSON.stringify(pickedColors));
+    document.querySelector(".picked-colors").classList.add("hide");
+}
+
+clearAll.addEventListener("click", clearAllColors);
+colorPickerBtn.addEventListener("click", activateEyeDropper);
